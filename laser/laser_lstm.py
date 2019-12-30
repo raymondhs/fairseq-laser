1f7e5798826363e8b31f4cf7c79a03bf9f000405
Raymond Hendy <raymhs91@gmail.com>
2019-12-30 13:26:25 +0800

    Update args for load_state_dict()

--- laser/laser_lstm.py
+++ laser/laser_lstm.py
@@ -234,14 +234,14 @@ class LaserLSTMModel(FairseqMultiModel):
             decoders[lang_pair] = decoder
         return LaserLSTMModel(encoders, decoders)
 
-    def load_state_dict(self, state_dict, strict=True):
+    def load_state_dict(self, state_dict, strict=True, args=None):
         state_dict_subset = state_dict.copy()
         for k, _ in state_dict.items():
             assert k.startswith('models.')
             lang_pair = k.split('.')[1]
             if lang_pair not in self.models:
                 del state_dict_subset[k]
-        super().load_state_dict(state_dict_subset, strict=strict)
+        super().load_state_dict(state_dict_subset, strict=strict, args=None)
 
 
 @register_model_architecture('laser_lstm', 'laser_lstm')
